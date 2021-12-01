9ca947a13bbd6d41e26ae4d4eb0e0d1ebc7d74e9
Linas Galumbauskas <galumbauskasl@gmail.com>
2021-12-01 08:34:13 +0000

    html-4 files pakoreguotas

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,4 +1,43 @@
-let a = 1;
-let tekstas = 'Kintamojo a reiksme: ';
+// let a = 1;
+// let tekstas = 'Kintamojo a reiksme: ';
 
-console.log(tekstas + a);
+// console.log(tekstas + a);
+
+
+
+// let metai = 1986 % 10;
+// let menuo = 08 % 10;
+// let diena = 14 % 10;
+// let suma;
+
+
+// suma = metai + menuo + diena;
+// console.log(suma);
+
+
+// let metai = '1986';
+// let menuo = '08';
+// let diena = '14';
+// let suma;
+
+// suma = (metai[metai.length-1]*1) + (menuo[menuo.length-1]*1) + (diena[diena.length-1]*1);
+
+// console.log(suma);
+
+
+// let gimimoData = [1986, 08, 14];
+// let suma = gimimoData.map((x) => x%10).reduce((suma, n) => suma + n)
+
+// console.log(suma);
+
+let a = -5;
+let b = 0;
+let c = -4;
+let d = 4;
+let x = -4;
+let y = 0;
+
+
+if(a < b && c < d) {
+  console.log(x,y);
+}
